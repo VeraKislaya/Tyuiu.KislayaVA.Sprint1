048573501e80d13ae7e4ef38b7bc3c924190200f
KislayaVA <marlonsamurin@gmail.com>
2025-10-04 00:02:38 +0500

    Task4

---
+++ Tyuiu.KislayaVA.Sprint1.Task4.V8.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KislayaVA.Sprint1.Task4.V8.Lib;
+namespace Tyuiu.KislayaVA.Sprint1.Task4.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate(1, 1);
+            Assert.AreEqual(0.75, res);
+        }
+    }
+}
