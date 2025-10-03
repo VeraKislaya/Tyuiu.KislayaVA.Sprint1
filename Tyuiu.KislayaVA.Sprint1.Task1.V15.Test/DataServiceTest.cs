82db0bd9740f5cc8ad25cce953da80ed1a77e91f
KislayaVA <marlonsamurin@gmail.com>
2025-10-04 00:01:34 +0500

    Task1

---
+++ Tyuiu.KislayaVA.Sprint1.Task1.V15.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KislayaVA.Sprint1.Task1.V15.Lib;
+namespace Tyuiu.KislayaVA.Sprint1.Task1.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.5;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.KislayaVA.Sprint1.Task1.V15/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.KislayaVA.Sprint1.Task1.V15.Lib;
+
+namespace Tyuiu.KislayaVA.Sprint1.Task1.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Кислая В. А. | РППб25-1";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                    *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                             *");
+            Console.WriteLine("* Задание #1                                                                   *");
+            Console.WriteLine("* Вариант #15                                                                  *");
+            Console.WriteLine("* Выполнила: Кислая В. А. | РППб25-1                                           *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Написать консольную программу, которая запрашивает у пользователя исходные   *");
+            Console.WriteLine("* данные, вычисляет результат по формуле (4 + 2 * x) / 7 и печатает его        *");
+            Console.WriteLine("* на экране.                                                                   *");
+            Console.WriteLine("*                                                                              *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+
+            double x;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x));
+            Console.ReadLine();
+        }
+    }
+}
